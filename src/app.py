36e74fd2856fff2aec39ed1f71834fafa29e6a5a
Carlos Daniel Hurtado <108777036+carlosdaniel236@users.noreply.github.com>
2024-04-16 17:52:48 +0000

    Al fin funciona

--- src/app.py
+++ src/app.py
@@ -14,7 +14,32 @@ CORS(app)
 
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
+Jhon = {
+    "id": jackson_family._generateId(),
+    "name":"John Jackson",
+    "age": "33 Years old",
+    "lucky_numbers": [7, 13, 22]
+}
 
+jackson_family.add_member(Jhon)
+
+Jane = {
+    "id": jackson_family._generateId(),
+    "name":"Jane Jackson",
+    "age":"35 Years old",
+    "lucky_numbers": [10, 14, 3]
+}
+
+jackson_family.add_member(Jane)
+
+Jimmy = {
+    "id": jackson_family._generateId(),
+    "name": "Jimmy  Jackson",
+    "age": "5 Years old",
+    "lucky_numbers": 1
+}
+
+jackson_family.add_member(Jimmy)
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -27,18 +52,33 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+#creando endpoint post
+@app.route('/member', methods=['POST'])
+def add_member():  
+     new_member = request.json
+     success = jackson_family.add_member(new_member)
+     if success == True: 
+         return jsonify(new_member), 200
+     return jsonify(success), 400
+
+#creando endpoint delete
+@app.route('/member/<int:member_id>', methods=["DELETE"])
+def delete_member(member_id):
+    success = jackson_family.delete_member(member_id)
+    return jsonify({'done': True}), 200
+      
+    #buscar un solo miembro
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member(member_id):
+    one_member = jackson_family.get_member(member_id)
+    return jsonify(one_member), 200
+    
+    # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/datastructures.py
+++ src/datastructures.py
@@ -21,15 +21,32 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        id = member.get("id", None) 
+        if id != None: 
+            for person in self._members: 
+                if person.get("id") == id: 
+                    return {
+                        "msg": "Tu cedula era repetida asi que asigne una nueva"
+                    }
+        else: 
+            member["id"] = self._generateId() 
+
+        self._members.append(member)
+        return True
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+            for member in self._members:
+                if member['id'] == id:
+                    self._members.remove(member)
+                    return self._members  
+
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
